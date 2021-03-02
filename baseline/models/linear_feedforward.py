d45060761159804b25353a8071fe92f1d02d229e
aglettiere <allisonglettiere@gmail.com>
2021-03-01 18:27:34 -0800

    updated linear input size

--- baseline/models/linear_feedforward.py
+++ baseline/models/linear_feedforward.py
@@ -2,12 +2,12 @@ import torch
 import torch.nn as nn
 
 class LinearFeedForward(nn.Module):
-	def __init__(self, dataset, input_size):
+	def __init__(self, dataset):
 		super().__init__()
 		num_classes = 8 if dataset=="emotion" else 2
 		linear_layer_size = 2048
 		model = nn.Sequential(
-			nn.Linear(input_size, linear_layer_size),
+			nn.Linear(1, linear_layer_size),
 			nn.ReLU(inplace=True),
 			nn.Dropout(0.5),
 			nn.Linear(linear_layer_size, linear_layer_size),
