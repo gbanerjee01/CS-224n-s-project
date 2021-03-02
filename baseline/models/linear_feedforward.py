b0ebeed4deb14b8f9a45d4a4515c08dfabc6f5c3
Allison Lettiere <allisonglettiere@gmail.com>
2021-03-01 18:00:10 -0800

    added linear feedforward network

---
+++ baseline/models/linear_feedforward.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class LinearFeedForward(nn.Module):
+	def __init__(self, dataset, input_size):
+		super().__init__()
+		num_classes = 7 if dataset=="emotion" else 2
+		linear_layer_size = 2048
+		model = nn.Sequential(
+			nn.Linear(input_size, linear_layer_size),
+			nn.ReLU(inplace=True),
+			nn.Dropout(0.5),
+			nn.Linear(linear_layer_size, linear_layer_size),
+			nn.ReLU(inplace=True),
+			nn.Linear(linear_layer_size, num_classes))
+
+	def forward(self, x):
+		output = self.model(x)
+		return output
