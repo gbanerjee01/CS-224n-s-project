119bd0e0dd48d2bb81e6ab8330c8f1612f1a1d9c
aglettiere <allisonglettiere@gmail.com>
2021-03-01 19:32:39 -0800

    Update linear_feedforward.py

--- baseline/models/linear_feedforward.py
+++ baseline/models/linear_feedforward.py
@@ -7,7 +7,7 @@ class LinearFeedForward(nn.Module):
 		num_classes = 8 if dataset=="emotion" else 2
 		linear_layer_size = 2048
 		model = nn.Sequential(
-			nn.Linear(1, linear_layer_size),
+			nn.Linear(, linear_layer_size),
 			nn.ReLU(inplace=True),
 			nn.Dropout(0.5),
 			nn.Linear(linear_layer_size, linear_layer_size),
