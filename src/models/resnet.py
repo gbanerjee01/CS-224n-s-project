2471289d74b1d637f4dbc01d366a2e1b84fe12ef
gbanerjee01 <gaurab.banerjee97@gmail.com>
2021-03-12 02:57:23 -0700

    resnet config

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -5,10 +5,12 @@ import torchvision.models as models
 class ResNet(nn.Module):
 	def __init__(self, dataset, pretrained=True):
 		super(ResNet, self).__init__()
-		num_classes = 50 if dataset=="ESC" else 10
+		num_classes = 8
 		self.model = models.resnet50(pretrained=pretrained)
 		self.model.fc = nn.Linear(2048, num_classes)
 		
 	def forward(self, x):
-		output = self.model(x)
+		squeezed_input = torch.squeeze(torch.stack([x,x,x],dim=1))
+        #squeezed_input = torch.stack([x,x,x],dim=1)
+        output = self.model(squeezed_input)
 		return output
