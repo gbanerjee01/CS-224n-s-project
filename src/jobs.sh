0bca8c57560c9754e4853e2e752b61979928688d
gbanerjee01 <gaurab.banerjee97@gmail.com>
2021-03-12 21:41:45 -0700

    jobs

--- src/jobs.sh
+++ src/jobs.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-python train.py --config_path config/densenet/m_mfcc.json
+python train.py --config_path config/resnet/m_mfcc_tonnetz.json
 
-python train.py --config_path config/densenet/m_mfcc_sc.json
+python train.py --config_path config/resnet/m_mfcc_chromagram.json
 
-python train.py --config_path config/resnet/all5.json
+python train.py --config_path config/resnet/m_mfcc_sc.json
 
-python train.py --config_path config/resnet/m_mfcc.json
+python train.py --config_path config/densenet/m_mfcc_tonnetz.json
 
-python train.py --config_path config/resnet/spec_contrast.json
-
-python train.py --config_path config/resnet/chromagram.json
+python train.py --config_path config/densenet/m_mfcc_chromagram.json
