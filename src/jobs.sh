cb6212c1b4ec6969f04140f9d20670369418cd48
gbanerjee01 <gaurab.banerjee97@gmail.com>
2021-03-13 01:06:11 -0700

    m and mfcc config

--- src/jobs.sh
+++ src/jobs.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-python train.py --config_path config/simple_conv/M_chromagram_speccontrast_tonnetz.json
+python train.py --config_path config/simple_conv/M.json
 
-python train.py --config_path config/simple_conv/M_mfcc_chromagram_speccontrast.json
+python train.py --config_path config/simple_conv/mfcc.json
 
-python train.py --config_path config/simple_conv/M_mfcc_chromagram_speccontrast_tonnetz.json
+python train.py --config_path config/simple_conv/sc.json
 
-python train.py --config_path config/simple_conv/M_mfcc_chromagram_tonnetz.json
+python train.py --config_path config/simple_conv/chr.json
 
-python train.py --config_path config/simple_conv/M_mfcc_speccontrast_tonnetz.json
+python train.py --config_path config/simple_conv/ton.json
 
-python train.py --config_path config/simple_conv/mfcc_chromagram_speccontrast_tonnetz.json
+# python train.py --config_path config/simple_conv/M_chromagram_speccontrast_tonnetz.json
+
+# python train.py --config_path config/simple_conv/M_mfcc_chromagram_speccontrast.json
+
+# python train.py --config_path config/simple_conv/M_mfcc_chromagram_speccontrast_tonnetz.json
+
+# python train.py --config_path config/simple_conv/M_mfcc_chromagram_tonnetz.json
+
+# python train.py --config_path config/simple_conv/M_mfcc_speccontrast_tonnetz.json
+
+# python train.py --config_path config/simple_conv/mfcc_chromagram_speccontrast_tonnetz.json
