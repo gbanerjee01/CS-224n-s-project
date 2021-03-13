688606de7be455920ae8b4671f1a5dac02d4f01a
gbanerjee01 <gaurab.banerjee97@gmail.com>
2021-03-12 18:15:29 -0700

    jobs update

--- src/jobs.sh
+++ src/jobs.sh
@@ -8,8 +8,6 @@ python train.py --config_path config/resnet/all5.json
 
 python train.py --config_path config/resnet/m_mfcc.json
 
-python train.py --config_path config/resnet/m_mfcc.json
-
 python train.py --config_path config/resnet/spec_contrast.json
 
 python train.py --config_path config/resnet/chromagram.json
