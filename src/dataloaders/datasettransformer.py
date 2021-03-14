06799a2df7443265c6a58439a2c207d38e85f63f
Justin Wang <jjwang01@stanford.edu>
2021-03-13 23:51:42 -0800

    refactor transformer dataset implementation for multiple features

--- src/dataloaders/datasettransformer.py
+++ src/dataloaders/datasettransformer.py
@@ -6,11 +6,11 @@ import pickle
 import torch
 
 class TransformerDataset(Dataset):
-  def __init__(self, df):
+  def __init__(self, df, features):
     self.labels = df['emotion'].reset_index(drop=True)
-    self.num_labels = self.labels.nunique()
-    self.mels = df['mel_pad'].reset_index(drop=True)
-    self.max_len = self.mels[0].shape[1]
+    self.features = {}
+    for feat in features:
+        self.features[feat] = df[feat + '_pad'].reset_index(drop=True)
     self.image_size = 384
     self.transform = transforms.Compose([transforms.ToTensor(), 
                                           transforms.Resize((self.image_size, self.image_size))])
@@ -18,20 +18,24 @@ class TransformerDataset(Dataset):
                                                               #[0.5, 0.5, 0.5]),])
       
   def __len__(self):
-    return len(self.mels)
+    return len(self.labels)
   
   def __getitem__(self, idx):
     if torch.is_tensor(idx):
         idx = idx.tolist()
-        
-    mel = self.mels.iloc[idx]
+    
+    data = np.concatenate([self.features[feat].iloc[idx] for feat in self.features], axis=0)
+    # pad 1st dimension to the 0th dimension
+    padded_data = np.zeros((data.shape[0], data.shape[0]))
+    padded_data[:,:data.shape[1]] = data
+    img = self.transform(padded_data)
+    # stack the same data three times to emulate RGB image
+    img = torch.stack([img]*3, dim=1).squeeze(dim=0).type(torch.float)
+
     label = self.labels.iloc[idx]        
-    mel = self.transform(mel[:250,])
-    # stack the same mel spectrogram three times to emulate RGB image
-    mel = torch.stack([mel]*3, dim=1).squeeze(dim=0).type(torch.float)
     label = torch.tensor(label-1).type(torch.long)
     
-    return mel, label
+    return img, label
 
 def fetch_dataloader(df, batch_size, num_workers):
     dataset = TransformerDataset(df)
