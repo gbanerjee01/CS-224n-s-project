64aa01952bb8a123353e04670c130b854ec48e62
Justin Wang <jjwang01@stanford.edu>
2021-03-14 22:08:30 -0700

    run best transformer models for 400 epochs

--- src/transformer_jobs.sh
+++ src/transformer_jobs.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-for file in config/transformer/*.json; do
-    python train.py --config="$file"
-done
+#for file in config/transformer/*.json; do
+#    python train.py --config="$file"
+#done
+
+python train.py --config=transformer/all5_freeze_11_epoch_400.json
+python train.py --config=transformer/mel_mfcc_tonnetz_freeze_11_epoch_400.json
