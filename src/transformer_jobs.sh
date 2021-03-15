4701a83a19fb58adc5f0afcdebd402e1e431a68b
Justin Wang <jjwang01@stanford.edu>
2021-03-14 22:32:45 -0700

    adjust job scripts for transfomers

--- src/transformer_jobs.sh
+++ src/transformer_jobs.sh
@@ -4,5 +4,5 @@
 #    python train.py --config="$file"
 #done
 
-python train.py --config=transformer/all5_freeze_11_epoch_400.json
-python train.py --config=transformer/mel_mfcc_tonnetz_freeze_11_epoch_400.json
+python train.py --config=config/transformer/all5_freeze_11_epoch_400.json
+python train.py --config=config/transformer/mel_mfcc_tonnetz_freeze_11_epoch_400.json
