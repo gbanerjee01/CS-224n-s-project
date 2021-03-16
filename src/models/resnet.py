fde9e0690bda1828d5ffcac1cfa44266a0318b25
gbanerjee01 <gaurab.banerjee97@gmail.com>
2021-03-16 03:55:32 -0700

    tabs and spaces

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -11,15 +11,15 @@ class ResNet(nn.Module):
 
 		device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         #comment out below lines if not running test run
-        checkpoint = torch.load(checkpoint_path, map_location=torch.device(device))
-        loaded_dict = checkpoint['model']
-        prefix = 'model.'
-        n_clip = len(prefix)
-        adapted_dict = {k[n_clip:]: v for k, v in loaded_dict.items() if k.startswith(prefix)}
-        self.model.load_state_dict(adapted_dict)
+		checkpoint = torch.load(checkpoint_path, map_location=torch.device(device))
+		loaded_dict = checkpoint['model']
+		prefix = 'model.'
+		n_clip = len(prefix)
+		adapted_dict = {k[n_clip:]: v for k, v in loaded_dict.items() if k.startswith(prefix)}
+		self.model.load_state_dict(adapted_dict)
 
-        for param in self.model.parameters():
-            param.requires_grad = False
+		for param in self.model.parameters():
+			param.requires_grad = False
 		
 	def forward(self, x):
 		squeezed_input = torch.squeeze(torch.stack([x,x,x],dim=1))
